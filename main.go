6db91eb9b6e27288b2cb69bad48db6fe1013ef1e
Matias Cardenas <cardenasmatias.1990@gmail.com>
2024-08-10 01:23:51 +0200

    feat:  adding extra endpoints

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+)
+
+type apiConfig struct {
+	fileServerHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits++
+		next.ServeHTTP(w, r)
+	})
+}
 
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -8,17 +22,32 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func (cfg *apiConfig) hitsCountHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits: " + strconv.Itoa(cfg.fileServerHits)))
+}
+
+func (cfg *apiConfig) resetCountHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileServerHits = 0
+}
+
 func main() {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	cfg := &apiConfig{}
+	serveMux.Handle("/app/", cfg.middlewareMetricsInc(fileServerHandler))
 	// server file in ./assets/logo.png
 	serveMux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 	serveMux.HandleFunc("/healthz", readinessHandler)
+	serveMux.HandleFunc("/metrics", cfg.hitsCountHandler)
+	serveMux.HandleFunc("/reset", cfg.resetCountHandler)
 
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
-
 	server.ListenAndServe()
 }
